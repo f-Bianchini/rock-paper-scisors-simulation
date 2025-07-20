85c4aafbd6567631d74055d0e602eb30b696b5af
Bianchini <felipe.bianchini@ucr.ac.cr>
2025-07-19 19:50:31 -0600

    Added movement to the entities

--- src/Entity.py
+++ src/Entity.py
@@ -1,15 +1,17 @@
 import pygame
 
 class Entity(pygame.sprite.Sprite):
-    def __init__(self, filepath, position, velocity=(0, 0)):
+    def __init__(self, filepath, type, position, velocity):
         super().__init__()
         self.image = pygame.image.load(filepath).convert_alpha()
+        self.type = type
         self.rect = self.image.get_rect(topleft=position)
         self.velocity = pygame.math.Vector2(velocity)
+        self.pos = pygame.math.Vector2(position)
 
     def Update(self, dt):
-        self.rect.x += self.velocity.x * dt
-        self.rect.y += self.velocity.y * dt
+        self.pos += self.velocity * dt
+        self.rect.topleft = self.pos
 
-    def Draw(self, surface):
-        surface.blit(self.image, self.rect)
+    def Draw(self, screen):
+        screen.blit(self.image, self.rect)
--- src/simulation.py
+++ src/simulation.py
@@ -6,29 +6,41 @@ import Entity
 def createEntities(rocks, papers, scissors, entitiesVelocity):
     entities = []
     for i in range(rocks):
-        entity = Entity.Entity("./assets/rock.png", (random.randint(0,100), random.randint(0,100)), entitiesVelocity)
+        direction1 = random.randint(0,1)
+        direction2 = random.randint(0,1)
+        entitiesVel = (entitiesVelocity if direction1 == 0 else entitiesVelocity * -1, 
+                       entitiesVelocity if direction2 == 0 else entitiesVelocity * -1)
+        entity = Entity.Entity("./assets/rock.png", "rock", (random.randint(0,100), random.randint(0,100)), entitiesVel)
         entities.append(entity)
     for i in range(papers):
-        entity = Entity.Entity("./assets/paper.png", (random.randint(300,500), random.randint(300,500)), entitiesVelocity)
+        direction1 = random.randint(0,1)
+        direction2 = random.randint(0,1)
+        entitiesVel = (entitiesVelocity if direction1 == 0 else entitiesVelocity * -1, 
+                       entitiesVelocity if direction2 == 0 else entitiesVelocity * -1)
+        entity = Entity.Entity("./assets/paper.png", "paper", (random.randint(300,500), random.randint(300,500)), entitiesVel)
         entities.append(entity)
     for i in range(scissors):
-        entity = Entity.Entity("./assets/scissors.png", (random.randint(500,700), random.randint(500,700)), entitiesVelocity)
+        direction1 = random.randint(0,1)
+        direction2 = random.randint(0,1)
+        entitiesVel = (entitiesVelocity if direction1 == 0 else entitiesVelocity * -1, 
+                       entitiesVelocity if direction2 == 0 else entitiesVelocity * -1)
+        entity = Entity.Entity("./assets/scissors.png", "scissors", (random.randint(500,700), random.randint(500,700)), entitiesVel)
         entities.append(entity)
     return entities
 
 pygame.init()
 screen = pygame.display.set_mode((960, 700))
 clock = pygame.time.Clock()
-entitiesVelocity = (10,10)
+entitiesVelocity = 20
 entities = createEntities(20, 20, 20, entitiesVelocity)
 running = True
-dt = 0
 
 while running:
     dt = clock.tick(60) / 1000
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    screen.fill((0, 0, 0))
     for entity in entities:
         entity.Update(dt)
         entity.Draw(screen)
